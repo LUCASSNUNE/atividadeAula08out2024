55f2f1d3a7592624c844a2b8a046f31d92fc7cde
LUCASSNUNE <20232223809lucas@gmail.com>
2024-10-08 09:47:03 -0300

    Add files via upload

---
+++ lucasEletroG7out.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  
+        int idadeLucas = 20;
+        int idadeBianca = 20;
+
+        if (idadeLucas == idadeBianca){
+        printf ("Bianca tem mesma idade de lucas");
+        }
+        else {
+        printf ("lucas nao tem idade igual bianca");
+        }
+    
+}
+
+
